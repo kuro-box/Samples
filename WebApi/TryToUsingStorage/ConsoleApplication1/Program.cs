305f434c635bfd62ecbd106b54005f32ef0416c9
kuro <kuro_q@outlook.com>
2016-05-31 00:10:19 +0800

    尝试在模拟器中使用StorageQueue

---
+++ WebApi/TryToUsingStorage/ConsoleApplication1/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Retrieve storage account information from connection string.
+            CloudStorageAccount storageAccount = CreateStorageAccountFromConnectionString(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create a queue client for interacting with the queue service
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            Console.WriteLine("1. Create a queue for the demo");
+            CloudQueue queue = queueClient.GetQueueReference("samplequeue");
+            queue.CreateIfNotExists();
+            try
+            {
+                //await queue.CreateIfNotExistsAsync();
+            }
+            catch (StorageException ex)
+            {
+                Console.WriteLine("If you are running with the default configuration please make sure you have started the storage emulator. Press the Windows key and type Azure Storage to select and run it from the list of applications - then restart the sample.");
+                Console.ReadLine();
+                throw;
+            }
+
+            //return queue;
+        }
+        private static CloudStorageAccount CreateStorageAccountFromConnectionString(string storageConnectionString)
+        {
+            CloudStorageAccount storageAccount;
+            try
+            {
+                storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid storage account information provided. Please confirm the AccountName and AccountKey are valid in the app.config file - then restart the sample.");
+                Console.ReadLine();
+                throw;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid storage account information provided. Please confirm the AccountName and AccountKey are valid in the app.config file - then restart the sample.");
+                Console.ReadLine();
+                throw;
+            }
+
+            return storageAccount;
+        }
+    }
+}
---
+++ WebApi/TryToUsingStorage/WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("api")]
+    public class DefaultController : ApiController
+    {
+        private static CloudStorageAccount CreateStorageAccountFromConnectionString(string storageConnectionString)
+        {
+            CloudStorageAccount storageAccount;
+            try
+            {
+                storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid storage account information provided. Please confirm the AccountName and AccountKey are valid in the app.config file - then restart the sample.");
+                Console.ReadLine();
+                throw;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid storage account information provided. Please confirm the AccountName and AccountKey are valid in the app.config file - then restart the sample.");
+                Console.ReadLine();
+                throw;
+            }
+
+            return storageAccount;
+        }
+        [Route]
+        public IHttpActionResult Get()
+        {
+            // Retrieve storage account information from connection string.
+            CloudStorageAccount storageAccount = CreateStorageAccountFromConnectionString(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create a queue client for interacting with the queue service
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            Console.WriteLine("1. Create a queue for the demo");
+            CloudQueue queue = queueClient.GetQueueReference("samplequeue");
+            queue.CreateIfNotExists();
+            try
+            {
+                //await queue.CreateIfNotExistsAsync();
+            }
+            catch (StorageException ex)
+            {
+                Console.WriteLine("If you are running with the default configuration please make sure you have started the storage emulator. Press the Windows key and type Azure Storage to select and run it from the list of applications - then restart the sample.");
+                Console.ReadLine();
+                throw;
+            }
+
+            return Ok("Helloworld");
+        }
+
+        [Route("{id:int}")]
+        public IHttpActionResult Post(int id)
+        {
+            var guid = Guid.NewGuid().ToString();
+
+            //var storageAccount = CloudStorageAccount.DevelopmentStorageAccount;
+            //var storageConnectionString = CloudConfigurationManager.GetSetting("Microsoft.WindowsAzure.Plugins.Diagnostics.ConnectionString");
+            var storageConnectionString = CloudConfigurationManager.GetSetting("StorageConnectionString");
+            var storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+            var queueStorage = storageAccount.CreateCloudQueueClient();
+
+            var queue = queueStorage.GetQueueReference("testqueue");
+            queue.CreateIfNotExists();
+
+            var msg = string.Format("id:{0}; key:{1}", id, guid);
+
+            queue.AddMessage(new CloudQueueMessage(msg));
+
+            var msgs = queue.GetMessages(3);
+
+            return Ok(msgs);
+        }
+    }
+}
