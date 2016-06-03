8b1bb9af54cb3bdcf5d157f1647be2d0792a9dad
kuro <kuro_q@outlook.com>
2016-06-03 16:39:10 +0800

    更新

---
+++ DesignPattern/DesignPatternSamples/AbstractFactory.App.Bootstarp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory.App.Bootstarp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class Road
+        {
+            public string Id { get; set; }
+
+            public string Code { get; set; }
+        }
+
+        public class RoadFactory
+        {
+            public static Road CreateRoad()
+            {
+                return new Road();
+            }
+        }
+    }
+}
--- DesignPattern/DesignPatternSamples/SingletonPattern.App.Bootstarp/Program.cs
+++ DesignPattern/DesignPatternSamples/SingletonPattern.App.Bootstarp/Program.cs
@@ -6,8 +6,8 @@ namespace SingletonPattern.App.Bootstarp
     {
         static void Main(string[] args)
         {
-            var i1 = SingleThreadSingleton.Instance1;
-            var i2 = SingleThreadSingleton.Instance1;
+            var i1 = Singleton.Instance;
+            var i2 = Singleton.Instance;
 
             Console.WriteLine(i1.Equals(i2));
 
@@ -15,42 +15,73 @@ namespace SingletonPattern.App.Bootstarp
         }
     }
 
-    /* 单件模式(Singleton Pattern)
-     * 动机(Motivation)
-     * 有些类，需要确保其在系统中只存在一个实例
-     * 优点：简洁，易懂
-     * 缺点：无法实现带参数的实例的创建
-     */
-    public class SingleThreadSingleton
+    /// <summary>
+    /// 单例模式的实现
+    /// </summary>
+    public class Singleton
     {
-        private SingleThreadSingleton() { }//这里设置构造函数为私有，确保无法多次构造
+        /// <summary>
+        /// 定义私有的构造函数，禁止创建实例
+        /// </summary>
+        private Singleton() { }
+
+#pragma warning disable
+        private int testNumber = 0;
+#pragma warning restore
 
         #region 实现方式1
-        private static object lockObject = new object();//用于确保线程安全
+        //// 定义一个标识确保线程同步
+        //private static readonly object locker = new object();
 
-        private static volatile SingleThreadSingleton instance1 = null;//不适用volatile关键字，依然可能发生线程不安全，这里的作用是DoubleCheck
+        //// 定义一个静态变量来保存类的实例，volatile关键字用于确保线程安全
+        //private static volatile Singleton instance = null;
 
-        public static SingleThreadSingleton Instance1
-        {
-            get
-            {
-                lock (lockObject)
-                {
-                    if (instance1 == null)
-                        instance1 = new SingleThreadSingleton();
-                }
+        ///// <summary>
+        ///// 创建实例，此处要确保线程安全
+        ///// </summary>
+        ///// <returns></returns>
+        //private static Singleton InitializationInstance(int num = 999)
+        //{
+        //    // 这里的判断并非没有意义
+        //    // 没有这个判断第二次进来的时候，需要先lock，再判断
+        //    // 这个lock的意义，在与之后的访问不需要再进lock了
+        //    if (instance == null)
+        //    {
+        //        lock (locker)
+        //        {
+        //            if (instance == null)
+        //                instance = new Singleton { testNumber = num };
+        //        }
+        //    }
 
-                return instance1;
-            }
-        }
+        //    return instance;
+        //}
+
+        ///// <summary>
+        ///// 定义公共的属性来提供一个全局的访问入口，适用于无参数构建实例
+        ///// </summary>
+        //public static Singleton Instance
+        //{
+        //    get { return InitializationInstance(); }
+        //}
+
+        ///// <summary>
+        ///// 定义公共的函数来提供一个全局的访问入口，适用于带参数构建实例
+        ///// </summary>
+        ///// <returns></returns>
+        //public static Singleton GetInstance(int num)
+        //{
+        //    return InitializationInstance(num);
+        //}
         #endregion
 
         #region 实现方式2
-        public static readonly SingleThreadSingleton instance2 = null;
+        // 定义公共的变量来提供一个全局的访问入口，适用于无参数构建实例
+        public static readonly Singleton Instance = null;
 
-        static SingleThreadSingleton()
+        static Singleton()
         {
-            instance2 = new SingleThreadSingleton();
+            Instance = new Singleton { testNumber = 999 };
         }
         #endregion
     }
